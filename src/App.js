c606f573da24822885171def09a9da9b170e4c7e
SAT853 <sathishrpsg671@gmail.com>
2020-05-02 00:36:30 +0530

    Add basic comp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Test from "./Components/Test";
+const App = () => {
+  return (
+    <div>
+      <h1>React Test App</h1>
+      <Test />
+    </div>
+  );
+};
+
+export default App;
